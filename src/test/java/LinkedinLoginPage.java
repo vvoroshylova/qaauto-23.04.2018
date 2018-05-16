dda698d946ef4e4d2af6f84ed4b0dc15c60229a5
vvoroshylova <vikaposts1@gmail.com>
2018-05-16 14:59:51 +0300

    1) Fix Assert on .is Displayed() in successfulLoginTest() - Add public boolean method isSignInButtonDisplayed() into LinkedinLoginPage class - Call isSignInButtonDisplayed() method in Assert in test 2) Fix null pointer exception on webDriver variable - Create constructor method that will get webDriver value and set it inside LinkedinLoginPage class

---
+++ src/test/java/LinkedinLoginPage.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LinkedinLoginPage {
+
+    private WebDriver webDriver;
+
+    private WebElement loginField;
+    private WebElement passwordField;
+    private WebElement signInButton;
+
+    public LinkedinLoginPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+    private void initElements() {
+        loginField = webDriver.findElement ( By.id ( "login-email"));
+        passwordField = webDriver.findElement ( By.id ( "login-password" ));
+        signInButton = webDriver.findElement ( By.id ("login-submit"));
+            }
+    public void login(String email, String password) {
+        initElements ();
+        loginField.sendKeys ( email );
+        passwordField.sendKeys ( password );
+        signInButton.click ();
+    }
+
+    public boolean isSignInButtonDisplayed() {
+        return signInButton.isDisplayed ();
+    }
+
+    public String getCurrentPageTitle() {
+        return webDriver.getTitle ();
+    }
+
+ }
+
+
+
+
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,74 +3,86 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
-import static org.testng.Assert.assertEquals;
 
 public class LinkedinLoginTest {
+    WebDriver webDriver;
+
+    @BeforeMethod
+    public void before() {
+        webDriver = new FirefoxDriver();
+        webDriver.get("http://linkedin.com/");
+
+    }
 
     @Test
     public void successfulLoginTest() throws InterruptedException {
-            WebDriver webDriver = new FirefoxDriver();
-            webDriver.get("http://linkedin.com/");
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage (webDriver);
 
         String actualLoginPageTitle = webDriver.getTitle();
-
 
         Assert.assertEquals((actualLoginPageTitle),
                 "LinkedIn: Log In or Sign Up",
                 "Login page Title is wrong");
 
-        WebElement signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
-        signInButton.click();
-        Assert.assertFalse(signInButton.isEnabled(),
-                "Sign In button is Disabled");
+        linkedinLoginPage.login ("vikaposts1@gmail.com", "Iamnewhere");
 
-        WebElement loginField = webDriver.findElement(By.id("login-email"));
-        loginField.sendKeys("vikaposts1@gmail.com");
-        WebElement passwordField = webDriver.findElement(By.id("login-password"));
-        passwordField.sendKeys("Wrongpassword");
-        signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
-        signInButton.click();
-        sleep ( 3000);
+        Assert.assertTrue(linkedinLoginPage.isSignInButtonDisplayed(),
+                "Sign In button is not displayed");
 
-        Assert.assertFalse(webDriver.getPageSource().contains("Home"), "Valid credentials");
-
-
-        loginField = webDriver.findElement (By.id("login-email"));
-       loginField.sendKeys("vikaposts1@gmail.com");
-       passwordField = webDriver.findElement(By.id("login-password"));
-       passwordField.sendKeys("Iamnewhere");
-       signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
-       signInButton.click();
-
-     sleep ( 3000);
-
-        String actualTitle = webDriver.getTitle();
-        String expectedTitle = "LinkedInX";
-        assertEquals(expectedTitle,actualTitle,"failed because....");
-
-        Assert.assertTrue(signInButton.isDisplayed(),
-                "Sign In button is not Displayed");
-
+            sleep ( 3000);
         Assert.assertEquals(webDriver.getCurrentUrl(),
                 "https://www.linkedin.com/feed/",
                 "Home page url is wrong");
 
-        String actualHomePageTitle = webDriver.getTitle();
 
+        String actualHomePageTitle = webDriver.getTitle();
+        Assert.assertNotEquals(actualLoginPageTitle, actualHomePageTitle,
+                "Page title did not change after Sign In");
 Assert.assertTrue(webDriver.getTitle().contains("LinkedIn"),
 "Home page url is wrong.");
-
-Assert.assertNotEquals(actualLoginPageTitle, actualHomePageTitle,
-        "Page title did not change after Sign In");
-
-
 
         webDriver.close();
 
 
             }
 
+@Test
+public void negativeLoginTest() throws InterruptedException {
+    String actualLoginPageTitle = webDriver.getTitle();
+
+    Assert.assertEquals((actualLoginPageTitle),
+            "LinkedIn: Log In or Sign Up",
+            "Login page Title is wrong");
+
+    LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage (webDriver);
+        linkedinLoginPage.login("vikaposts1@gmail.com", "1");
+
+        Assert.assertTrue(linkedinLoginPage.isSignInButtonDisplayed(),
+                "Sign In button is not displayed");
+
+        sleep ( 3000);
+
+    String currentPageUrl = webDriver.getCurrentUrl();
+    String currentPageTitle = webDriver.getTitle();
+
+    Assert.assertEquals(currentPageUrl, "https://www.linkedin.com/uas/login-submit",
+            "Login-Submit page url is wrong");
+    Assert.assertEquals(currentPageTitle, "Sign In to LinkedIn",
+            "Login-Submit page Title is wrong");
+
+    WebElement errorMessage = webDriver.findElement(By.xpath("//div[@role='alert']"));
+
+    Assert.assertEquals(errorMessage.getText(),
+            "There were one or more errors in your submission. Please correct the marked fields below.",
+            "Wrong error message text displayed");
+}
+@AfterMethod
+public void after() {
+        webDriver.close();
+}
     }
