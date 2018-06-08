a5192878cba2b414b3305ba5f8d867f4abf18b24
vvoroshylova <vikaposts1@gmail.com>
2018-06-08 14:18:07 +0300

    Last Lesson

--- src/test/java/page/LinkedinLoginSubmitPage.java
+++ src/test/java/page/LinkedinLoginSubmitPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LinkedinLoginSubmitPage extends LinkedinBasePage {
+public abstract class LinkedinLoginSubmitPage extends LinkedinBasePage {
 
     @FindBy(xpath = "//div[@class='alert error']//strong")
     private WebElement errorMessage;
